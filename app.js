c3b1393a869e20430fc207e80b7a937b287a4f95
Laura Font <laurafontsalvador@gmail.com>
2020-05-31 22:21:10 +0200

    app and package

--- app.js
+++ app.js
@@ -13,13 +13,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get("/", function (req, res, next) {
   res.send("App is up and running");
 });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
